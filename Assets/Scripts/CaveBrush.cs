c03787fc02e8f2969ea0c0268771efcddff07c40
Andreas Järvelä <andreas.jarvela.github@gmail.com>
2018-09-13 15:40:11 +0200

    Create a cave brush and also begin implementation of a mining tool

--- Assets/Scripts/CaveBrush.cs
+++ Assets/Scripts/CaveBrush.cs
@@ -1,16 +1,89 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class CaveBrush : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace UnityEditor {
+
+
+    [CustomGridBrush(false, true, false, "Cave Brush")]
+    public class CaveBrush : GridBrushBase {
+
+        public const string k_WallLayerName = "Walls";
+        public const string k_FloorLayerName = "Floor";
+
+        public TileBase m_Wall;
+        public TileBase m_Floor;
+
+        /**
+         * Fetch the two existing Tilemap in the scene that separate walls and floor, based on position clicked
+         * place a floor and add walls around if no floor is there.
+         **/
+        public override void Paint(GridLayout grid, GameObject layer, Vector3Int position)
+        {
+            Tilemap walls = GetWall();
+            Tilemap floor = GetFloor();
+
+            if (walls != null && floor != null)
+            {
+                floor.SetTile(position, m_Floor);
+
+                if (walls.GetTile(position) == m_Wall)
+                {
+                    walls.SetTile(position, null);
+                }
+
+                for (int x = -1; x < 2; ++x)
+                {
+                    for (int y = -1; y < 2; ++y)
+                    {
+                        Vector3Int pos = new Vector3Int(position.x + x, position.y + y, position.z);
+                        if (floor.GetTile(pos) != m_Floor)
+                        {
+                            walls.SetTile(pos, m_Wall);
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+        public override void Erase(GridLayout grid, GameObject layer, Vector3Int position)
+        {
+            Tilemap walls = GetWall();
+            Tilemap floor = GetFloor();
+
+            if (walls != null && floor != null)
+            {
+                walls.SetTile(position, null);
+                floor.SetTile(position, null);
+            }
+        }
+
+
+        public static Tilemap GetWall()
+        {
+            GameObject go = GameObject.Find(k_WallLayerName);
+            return go != null ? go.GetComponent<Tilemap>() : null;
+        }
+
+        public static Tilemap GetFloor()
+        {
+            GameObject go = GameObject.Find(k_FloorLayerName);
+            return go != null ? go.GetComponent<Tilemap>() : null;
+        }
+
+        [MenuItem("Assets/Create/Cave Brush")]
+        public static void CreateBrush()
+        {
+            string path = EditorUtility.SaveFilePanelInProject("Save Cave Brush", "New Cave Brush", "Asset", "Save Cave Brush", "Assets");
+            if (path == "")
+                return;
+            AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<CaveBrush>(), path);
+        }
+    }
+
 }
---
+++ Assets/Scripts/MiningTool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MiningTool : MonoBehaviour {
+
+    public Tilemap walls;
+    public Tilemap toolEffects;
+
+    public TileBase miningEffect;
+    public TileBase wall;
+
+    public Grid grid;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int cellPosition = grid.WorldToCell(pos);
+            if (walls.GetTile(cellPosition) == wall)
+            {
+                toolEffects.SetTile(cellPosition, miningEffect);
+            }
+        }
+    }
+
+    
+}
