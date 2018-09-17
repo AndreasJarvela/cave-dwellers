87b4813b9c9050efe37df5e4d76036dae6a65f7a
Andreas Järvelä <andreas.jarvela.github@gmail.com>
2018-09-17 22:27:17 +0200

    More things

---
+++ Assets/Scripts/Initialize.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Initialize : MonoBehaviour {
+
+
+    public const int width = 150;
+    public const int height = 150;
+
+    public Tilemap walls;
+    public Tilemap floor;
+
+    public TileBase wallTile;
+    public TileBase floorTile;
+
+    // Use this for initialization
+    void Start() {
+        initializeMap();
+        AstarPath.active.Scan();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+
+    void initializeMap()
+    {
+        int[,] map = new int[width, height];
+        for (int x = -map.GetUpperBound(0) / 2; x < map.GetUpperBound(0) / 2; x++)
+        {
+            for (int y = -map.GetUpperBound(1) / 2; y < map.GetUpperBound(1) / 2; y++)
+            {
+                walls.SetTile(new Vector3Int(x, y, 0), wallTile);
+            }
+        }
+
+        for (int i = -1; i < 2; i++)
+        {
+            for (int j = -1; j < 2; j++)
+            {
+                floor.SetTile(new Vector3Int(i, j, 0), floorTile);
+                walls.SetTile(new Vector3Int(i, j, 0), null);
+            }
+        }
+    }
+
+
+}
--- Assets/Scripts/MiningTool.cs
+++ Assets/Scripts/MiningTool.cs
@@ -13,23 +13,35 @@ public class MiningTool : MonoBehaviour {
 
     public Grid grid;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButtonDown(0))
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int cellPosition = grid.WorldToCell(pos);
+
+        if (Input.GetMouseButton(0))
         {
-            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int cellPosition = grid.WorldToCell(pos);
             if (walls.GetTile(cellPosition) == wall)
             {
                 toolEffects.SetTile(cellPosition, miningEffect);
             }
         }
-    }
 
-    
+        if (Input.GetMouseButton(1))
+        {
+            if (toolEffects.GetTile(cellPosition) == miningEffect)
+            {
+                toolEffects.SetTile(cellPosition, null);
+            }
+
+        }
+
+    }
 }
